bece396d21bf482c34d64f2c0c6e3e6b320c405d
Hesham El Abd <30498915+HeshamElAbd@users.noreply.github.com>
2019-09-02 13:16:31 +0200

    Add files via upload
    Correcting a bug in calling the function of the Annotator, the solution was by adding a tf.reduced_mean to condense the output of the encoder sequence

--- Models/EncoderModels.py
+++ Models/EncoderModels.py
@@ -128,7 +128,7 @@ class Annotator(tf.keras.Model):
         the dense subnetwor predictions, for example Relu or sigmoid, default
         is None. 
         """
-        super(Modeler,self).__init__()
+        super(Annotator,self).__init__()
         
         self.return_attent_weights=return_attent_weights
         
@@ -151,10 +151,12 @@ class Annotator(tf.keras.Model):
         if not self.return_attent_weights:
             encoded_seq=self.encoder(x,training,mask)
             encoded_seq=self.dropout(encoded_seq,training)
+            encoded_seq=tf.reduce_mean(encoded_seq,axis=1)
             modelPredictionLogit=self.pred_logits(encoded_seq)
             return modelPredictionLogit
         else: 
             encoded_seq, attent_weights=self.encoder(x,training,mask)
             encoded_seq=self.dropout(encoded_seq,training)
+            encoded_seq=tf.reduce_mean(encoded_seq,axis=1)
             modelPredictionLogit=self.pred_logits(encoded_seq)
             return self.pred_logits(encoded_seq), attent_weights
